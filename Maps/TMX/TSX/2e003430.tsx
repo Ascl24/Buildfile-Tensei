bd3b3e6a2fd53adcb9f97bb86380686997ee18cf
nobleboy <63758789+Ascl24@users.noreply.github.com>
2022-01-04 20:26:04 -0600

    Initial commit, adds all of my local files

---
+++ Maps/TMX/TSX/2e003430.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="2e003430" tilewidth="16" tileheight="16" tilecount="1024" columns="32">
+ <image source="../../FE8 Tilest/2e003430.png" trans="ff00ff" width="512" height="512"/>
+</tileset>
---
+++ make_HACK FULL.cmd
@@ -0,0 +1,17 @@
+@echo off
+cd %~dp0
+copy "Clean.gba" New.gba
+
+cd "%~dp0Text"
+textprocess_v2 text_buildfile.event
+
+cd "%~dp0Tables"
+c2ea 
+
+cd "%~dp0Maps"
+tmx2ea
+
+cd "%~dp0Event Assembler"
+ColorzCore A FE8 "-output:%~dp0New.gba" "-input:%~dp0Master.event"
+
+pause
